d6ec0bf62ce46cfbe7373f50fdf6b8305bac05a5
Tinisi <jgrosz@tinisi.com>
2026-06-21 23:42:21 -0500

    making a state bucket for this example repo

--- terraform/init-terraform-state/bucket-for-state.tf
+++ terraform/init-terraform-state/bucket-for-state.tf
@@ -1,22 +1,22 @@
-resource "aws_s3_bucket" "tofu-template-example" {
-  bucket              = "example-bucket-for-state"
+resource "aws_s3_bucket" "deps-example" {
+  bucket              = "deps-example-bucket-for-state"
   bucket_prefix       = null
   force_destroy       = null
   object_lock_enabled = false
   tags = {
-    some-tag = "example"
+    repo = "deps-examples"
   }
 }
 
-resource "aws_s3_bucket_versioning" "tofu-template-example" {
-  bucket = aws_s3_bucket.tofu-template-example.id
+resource "aws_s3_bucket_versioning" "deps-example" {
+  bucket = aws_s3_bucket.deps-example.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "tofu-template-example" {
-  bucket = aws_s3_bucket.tofu-template-example.id
+resource "aws_s3_bucket_public_access_block" "deps-example" {
+  bucket = aws_s3_bucket.deps-example.id
 
   block_public_acls       = true
   block_public_policy     = true
