c2bdc24f4a1a4558d459ff371ae4fa10d575e5c1
Jason Grosz <grosz@testdouble.com>
2026-08-01 23:19:01 -0500

    Adding vendor arg

--- terraform/nonprod/example-bedrock-inference.tf
+++ terraform/nonprod/example-bedrock-inference.tf
@@ -1,7 +1,7 @@
 module "example_bedrock_inference" {
   source = "../modules/bedrock_inference"
 
-  attributes = { tier = "fast" }
+  attributes = { tier = "fast", vendor = "indie" }
   env-suffix = module.config.env-suffix
   env        = module.config.env
   repo       = module.config.repo
